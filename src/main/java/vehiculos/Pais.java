0669e98574e8946ca82fe6f137d01ef01b7413b5
jumartineze <jumartineze@unal.edu.co>
2021-11-30 21:21:44 -0500

    change

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -13,28 +13,4 @@ public class Pais {
 	public String getNombre() {return nombre;}
 	public void setNombre(String nombre) {this.nombre = nombre;}
 	
-	private static Pais masVendedor(ArrayList<Pais> paises) {
-		ArrayList<Integer> rep = new ArrayList<Integer>(); 
-		ArrayList<Pais> repP = new ArrayList<Pais>();
-		
-		for (int i=0; i<paises.size(); i++) {
-			int count = 0;
-			if (repP.contains(paises.get(i))==false) {
-				repP.add(paises.get(i));
-				for (int e=0; e<paises.size(); e++) {
-					if (paises.get(i) == paises.get(e)) {
-						count++;
-					}
-				}
-			}
-			rep.add(count);
-		}
-
-		int max = Collections.max(rep);
-		return repP.get(rep.indexOf(max));
-	}
-	
-	public static Pais paisMasVendedor() {
-		return masVendedor(Vehiculo.paises);
-	}
 }
